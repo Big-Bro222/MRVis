53bd2da2577678c8d17454d70b586ff73379b86a
Big-Bro222 <33903538+Big-Bro222@users.noreply.github.com>
2020-04-20 15:46:37 +0200

    sliderwithvalue

---
+++ Assets/MeshContoller.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeshContoller : MonoBehaviour
+{
+    bool isShow;
+    void Start()
+    {
+        isShow = false;
+    }
+
+    public void MeshControl()
+    {
+        MeshRenderer[] meshRenderers = GetComponentsInChildren<MeshRenderer>();
+        foreach(MeshRenderer meshRenderer in meshRenderers)
+        {
+            meshRenderer.enabled = isShow;
+        }
+
+        isShow = !isShow;
+    }
+}
