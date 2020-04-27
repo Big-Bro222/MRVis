0e3e0b3db4698979238b08ca2efb24799bb2c14b
Big-Bro222 <33903538+Big-Bro222@users.noreply.github.com>
2020-04-27 13:24:34 +0200

    ScatterDice Update

--- Assets/Build-in scene/ScaterPlot/DataPoint.cs
+++ Assets/Build-in scene/ScaterPlot/DataPoint.cs
@@ -26,7 +26,7 @@ public class DataPoint : MonoBehaviour
         UpdateNum = Convert.ToInt32(0.08f / Time.deltaTime);
         delayNum = 0;
 
-        label = transform.parent.parent.Find("Label").gameObject;
+        label = transform.parent.parent.Find("InfoPanel").gameObject;
 
         if (calories < 100)
         {
--- Assets/DiceBehaviorHandler.cs
+++ Assets/DiceBehaviorHandler.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -21,7 +22,7 @@ public class DiceBehaviorHandler : MonoBehaviour
         ThreeDimension,
         Fat_Sugar,
         Fat_Protein,
-        Sugar_Protein,
+        Protein_Sugar,
     };
 
     public State state;
@@ -29,17 +30,63 @@ public class DiceBehaviorHandler : MonoBehaviour
     {
         state = State.ThreeDimension;
         dataPointOriginalPosition = new Vector3[dataPointFolder.childCount];
-        for (int i = 0; i < dataPointFolder.childCount; i++)
+        for (int i = 0; i < dataPointFolder.childCount-1; i++)
         {
             dataPointOriginalPosition[i] = dataPointFolder.GetChild(i).localPosition;
 
         }
     }
 
-    public void Translate( string state )
+    public void Translate( string stateStr )
     {
-        ThreeDimentionToProteinSugar();
-        
+        string[] stateArray = { "ThreeDimension", "Fat_Sugar", "Fat_Protein", "Sugar_Protein" };
+        string[] functionArray = { "", "ThreeDimentionToFatSugar", "ThreeDimentionToFatProtein", "ThreeDimentionToProteinSugar" };
+        int index = Array.IndexOf(stateArray, stateStr);
+        if (index == (int)state)
+        {
+            return;
+        }
+
+        switch (state)
+        {
+            case State.ThreeDimension:
+                break;
+            case State.Fat_Sugar:
+                FatSugarToThreeDimension();
+                break;
+            case State.Fat_Protein:
+                FatProteinToThreeDimention();
+                break;
+            case State.Protein_Sugar:
+                ProteinSugarToThreeDimension();
+                break;
+            default:
+                break;
+        }
+
+
+        switch (stateStr)
+        {
+            case "ThreeDimension":
+                state = State.ThreeDimension;   
+                break;
+            case "Fat_Sugar":
+                state = State.Fat_Sugar;
+
+                break;
+            case "Fat_Protein":
+                state = State.Fat_Protein;
+                break;
+            case "Protein_Sugar":
+                state = State.Protein_Sugar;
+                break;
+            default:
+                state = State.ThreeDimension;
+                break;
+        }
+
+        Invoke(functionArray[(int)state], 3);
+
     }
 
     public void Translatete(string state)
@@ -50,7 +97,7 @@ public class DiceBehaviorHandler : MonoBehaviour
 
     private void ThreeDimentionToFatProtein()
     {
-        for (int i = 0; i < dataPointFolder.childCount; i++)
+        for (int i = 0; i < dataPointFolder.childCount-1; i++)
         {
             Vector3 dataPointLocalPos = dataPointFolder.GetChild(i).localPosition;
             Vector3 newPos = new Vector3(dataPointLocalPos.x, dataPointLocalPos.y, 0.5f);
@@ -63,7 +110,7 @@ public class DiceBehaviorHandler : MonoBehaviour
 
     private void FatProteinToThreeDimention()
     {
-        for (int i = 0; i < dataPointFolder.childCount; i++)
+        for (int i = 0; i < dataPointFolder.childCount-1; i++)
         {
             Vector3 dataPointOriginalPos = dataPointOriginalPosition[i];
             Vector3 newPos = new Vector3(dataPointOriginalPos.x, dataPointOriginalPos.y, dataPointOriginalPos.z);
@@ -78,7 +125,7 @@ public class DiceBehaviorHandler : MonoBehaviour
     {
         StartCoroutine(RotateToRotation(new Vector3(0, -90, 0), rotationDuration));
 
-        for (int i = 0; i < dataPointFolder.childCount; i++)
+        for (int i = 0; i < dataPointFolder.childCount-1; i++)
         {
             Vector3 dataPointLocalPos = dataPointFolder.GetChild(i).localPosition;
             Vector3 newPos = new Vector3(0.5f, dataPointLocalPos.y, dataPointLocalPos.z);
@@ -92,7 +139,7 @@ public class DiceBehaviorHandler : MonoBehaviour
     private void FatSugarToThreeDimension()
     {
         StartCoroutine(RotateToRotation(new Vector3(0, 90, 0), rotationDuration));
-        for (int i = 0; i < dataPointFolder.childCount; i++)
+        for (int i = 0; i < dataPointFolder.childCount-1; i++)
         {
             Vector3 dataPointOriginalPos = dataPointOriginalPosition[i];
             Vector3 newPos = new Vector3(dataPointOriginalPos.x, dataPointOriginalPos.y, dataPointOriginalPos.z);
@@ -106,7 +153,7 @@ public class DiceBehaviorHandler : MonoBehaviour
     private void ThreeDimentionToProteinSugar()
     {
         StartCoroutine(RotateToRotation(new Vector3(0, -90, 90), rotationDuration));
-        for (int i = 0; i < dataPointFolder.childCount; i++)
+        for (int i = 0; i < dataPointFolder.childCount-1; i++)
         {
             Vector3 dataPointLocalPos = dataPointFolder.GetChild(i).localPosition;
             Vector3 newPos = new Vector3(dataPointLocalPos.x, -0.5f, dataPointLocalPos.z);
@@ -119,7 +166,7 @@ public class DiceBehaviorHandler : MonoBehaviour
     private void ProteinSugarToThreeDimension()
     {
         StartCoroutine(RotateToRotation(new Vector3(0, 90, -90), rotationDuration));
-        for (int i = 0; i < dataPointFolder.childCount; i++)
+        for (int i = 0; i < dataPointFolder.childCount-1; i++)
         {
             Vector3 dataPointOriginalPos = dataPointOriginalPosition[i];
             Vector3 newPos = new Vector3(dataPointOriginalPos.x, dataPointOriginalPos.y, dataPointOriginalPos.z);
