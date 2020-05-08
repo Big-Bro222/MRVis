0dcb186f87e3b27ce652104c9ad05a0198aaf5b2
Big-Bro222 <33903538+Big-Bro222@users.noreply.github.com>
2020-05-08 08:09:34 +0200

    Remote testing

---
+++ Assets/Wall/Scripts/WallRemove/CanvasEventHandler.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace WallRemote
+{
+    public class CanvasEventHandler : MonoBehaviour
+    {
+        public Camera cameraFirstPerson;
+        public GameObject cameraThirdPerson;
+
+        void Start()
+        {
+            cameraFirstPerson.enabled=true;
+            cameraThirdPerson.SetActive(false);
+        }
+
+        public void OnChangeView()
+        {
+            cameraThirdPerson.SetActive(!cameraThirdPerson.activeSelf);
+            cameraFirstPerson.enabled = !cameraFirstPerson.enabled;
+        }
+    }
+}
